b48f0501ed0279eb04a9a0e9779a19a9c2334ab0
asraarealty <155309749+asraarealty@users.noreply.github.com>
2026-06-14 19:55:11 +0530

    Update ThreeScene.tsx

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -2,64 +2,31 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { memo } from "react";
 
-type FloorProps = {
-  position: [number, number, number];
-};
-
-const Floor = memo(function Floor({ position }: FloorProps) {
+function Building() {
   return (
-    <mesh position={position}>
-      <boxGeometry args={[2, 0.3, 2]} />
-      <meshStandardMaterial color="#f97316" />
+    <mesh rotation={[0.2, 0.5, 0]}>
+      <boxGeometry args={[2, 5, 2]} />
+      <meshStandardMaterial color="#f59e0b" />
     </mesh>
   );
-});
+}
 
 export default function ThreeScene() {
   return (
-    <Canvas
-      style={{
-        position: "absolute",
-        inset: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: 0,
-      }}
-      camera={{
-        position: [3, 5, 8],
-        fov: 60,
-      }}
-      dpr={[1, 1.5]}
-      gl={{
-        antialias: true,
-        alpha: true,
-      }}
-    >
-      {/* Ambient Light */}
-      <ambientLight intensity={0.8} />
+    <div className="absolute inset-0">
+      <Canvas camera={{ position: [5, 3, 8] }}>
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 5, 5]} />
 
-      {/* Directional Light */}
-      <directionalLight position={[5, 5, 5]} intensity={1.2} />
+        <Building />
 
-      {/* Building Floors */}
-      {Array.from({ length: 8 }).map((_, i) => (
-        <Floor
-          key={i}
-          position={[0, i * 0.35, 0]}
+        <OrbitControls
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={1}
         />
-      ))}
-
-      {/* Controls */}
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={1}
-        enableZoom={false}
-        enablePan={false}
-        minPolarAngle={Math.PI / 4}
-        maxPolarAngle={Math.PI / 2}
-      />
-    </Canvas>
+      </Canvas>
+    </div>
   );
 }
