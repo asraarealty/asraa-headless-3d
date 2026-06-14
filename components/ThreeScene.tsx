37dcd6f9b30a500ea4b67b4114c176c018661d4c
asraarealty <155309749+asraarealty@users.noreply.github.com>
2026-06-14 18:52:51 +0530

    Update ThreeScene.tsx

--- components/ThreeScene.tsx
+++ components/ThreeScene.tsx
@@ -2,34 +2,64 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { memo } from "react";
 
-function Floor({ position }) {
+type FloorProps = {
+  position: [number, number, number];
+};
+
+const Floor = memo(function Floor({ position }: FloorProps) {
   return (
     <mesh position={position}>
       <boxGeometry args={[2, 0.3, 2]} />
       <meshStandardMaterial color="#f97316" />
     </mesh>
   );
-}
+});
 
 export default function ThreeScene() {
   return (
     <Canvas
       style={{
-        position: "fixed",
+        position: "absolute",
         inset: 0,
+        width: "100%",
+        height: "100%",
         zIndex: 0,
       }}
-      camera={{ position: [3, 5, 8], fov: 60 }}
+      camera={{
+        position: [3, 5, 8],
+        fov: 60,
+      }}
+      dpr={[1, 1.5]}
+      gl={{
+        antialias: true,
+        alpha: true,
+      }}
     >
-      <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+      {/* Ambient Light */}
+      <ambientLight intensity={0.8} />
 
-      {[...Array(8)].map((_, i) => (
-        <Floor key={i} position={[0, i * 0.35, 0]} />
+      {/* Directional Light */}
+      <directionalLight position={[5, 5, 5]} intensity={1.2} />
+
+      {/* Building Floors */}
+      {Array.from({ length: 8 }).map((_, i) => (
+        <Floor
+          key={i}
+          position={[0, i * 0.35, 0]}
+        />
       ))}
 
-      <OrbitControls autoRotate />
+      {/* Controls */}
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={1}
+        enableZoom={false}
+        enablePan={false}
+        minPolarAngle={Math.PI / 4}
+        maxPolarAngle={Math.PI / 2}
+      />
     </Canvas>
   );
 }
